abeff53be5039d6357f7a8fdb3e41abe643cd783
u46cm8 <touch2who@gmail.com>
2016-10-09 15:44:35 +0100

    Comments added and ReadME file too

--- Options.java
+++ Options.java
@@ -1,5 +1,3 @@
-
-
 // This is a program to make a choice of subject to Study
 import java.util.Random;
 
@@ -9,10 +7,13 @@ public class SubjectChoice {
 		// TODO Auto-generated method stub
 		
 		String[] subjects = new String[]{"java", "Maths","intro to comps and nets","info sys"};
+		//list of subjects made into an array 
 		
 		Random rand = new Random();
 		
 		int randomSelection = rand.nextInt(3- 0 +1) + 0;
+		
+		// Random number is used to select the index of a subject
 		
 		System.out.println(subjects[randomSelection]);
 		
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Options
-A program to select a study subject at random
+ Options
+
+
+This is a program to select a study subject at random as it is something I struggle with of and on. 
+
+There are limitations to the program as I initaly wrote on python, but since then I am keen to learn java in time I 
+hope to add more features. 
+
+
+
+What way better to use my procastination that to test what i know.
